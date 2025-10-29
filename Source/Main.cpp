9aad6edb222b29eac797c8b8e4bda3ee14aef314
Rich Elmes <rich.elmes@a-i-media.co.uk>
2025-10-29 19:18:23 +0100

    Initial commit.  Arguments parsed to show full-screen or config window.

---
+++ Source/Main.cpp
@@ -0,0 +1,80 @@
+#include <windows.h>  // <-- Add this first!
+#define WIN32_LEAN_AND_MEAN
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include "SaverComponent.h"
+#include "ConfigComponent.h"
+
+class Test : public juce::Component {
+public:
+    ~Test() {
+      logger.logMessage("exit");
+    }
+
+#if JUCE_DEBUG
+    juce::FileLogger logger { juce::File::getSpecialLocation(juce::File::SpecialLocationType::userDesktopDirectory).getChildFile("screensaver.log"),
+                              "ScreenSaver log" };
+#endif
+};
+
+class ScreensaverApplication : public juce::JUCEApplication
+{
+public:
+    const juce::String getApplicationName() override       { return JUCE_APPLICATION_NAME_STRING; }
+    const juce::String getApplicationVersion() override    { return JUCE_APPLICATION_VERSION_STRING; }
+    bool moreThanOneInstanceAllowed() override             { return true; }
+
+    void initialise (const juce::String& commandLine) override
+    {
+        if (commandLine.isEmpty() || commandLine.startsWith("/s"))
+        {
+            runFullScreen();
+        }
+        else if (commandLine.startsWith("/c"))
+        {
+            showConfigDialog();
+        }
+        else if (commandLine.startsWith("/p"))
+        {
+            quit();
+        }
+        else
+        {
+            runFullScreen();
+        }
+    }
+
+    void shutdown() override
+    {
+        saver = nullptr;
+        config = nullptr;
+    }
+
+    void systemRequestedQuit() override
+    {
+        quit();
+    }
+private:
+    std::unique_ptr<SaverComponent> saver;
+    std::unique_ptr<ConfigWindow> config;
+
+    void runFullScreen()
+    {
+        auto mainScreen = juce::Desktop::getInstance().getDisplays().getMainDisplay();
+        juce::Rectangle<int> screenBounds = mainScreen.totalArea;
+
+        saver = std::make_unique<SaverComponent>(false, [this] { quit(); });
+        saver->setOpaque(true);
+        saver->setBounds(screenBounds);
+        saver->addToDesktop(0);
+        saver->setVisible(true);
+        saver->toFront(true);
+    }
+
+    void showConfigDialog()
+    {
+        config.reset(new ConfigWindow(getApplicationName()));
+    }
+};
+
+START_JUCE_APPLICATION(ScreensaverApplication)
---
+++ Source/SaverComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
+
+class SaverComponent : public juce::Component,
+                       private juce::Timer
+{
+public:
+    explicit SaverComponent(bool isPreview, std::function<void()> onExitFn)
+        : onExit(std::move(onExitFn))
+        , preview(isPreview)
+    {
+        setWantsKeyboardFocus(true);
+        setMouseClickGrabsKeyboardFocus(true);
+        if (!preview) setMouseCursor(juce::MouseCursor::NoCursor);
+        startTimerHz(60);
+        lastMousePos = juce::Desktop::getInstance().getMainMouseSource().getScreenPosition();
+    }
+
+    void paint(juce::Graphics& g) override
+    {
+        g.fillAll(juce::Colours::black);
+        g.setColour(juce::Colours::white);
+        g.setFont(40.0f);
+        g.drawFittedText("JUCE Screensaver", getLocalBounds(), juce::Justification::centred, 1);
+    }
+
+private:
+    juce::Point<float> lastMousePos;
+    std::function<void()> onExit;
+    bool preview;
+
+    void timerCallback() override
+    {
+        auto mousePos = juce::Desktop::getInstance().getMainMouseSource().getScreenPosition();
+        if (!preview && mousePos.getDistanceFrom(lastMousePos) > 5.0f)
+            triggerExit();
+
+        repaint();
+    }
+
+    void mouseDown(const juce::MouseEvent&) override   { if (!preview) triggerExit(); }
+    bool keyPressed(const juce::KeyPress&) override    { if (!preview) triggerExit(); return !preview; }
+
+    void triggerExit()
+    {
+        if (onExit)
+            onExit();
+    }
+};
